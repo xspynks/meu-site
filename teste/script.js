94b81d3dc57fc93c4b42bf2081c1366d085b4fba
Rafael de Paula <xspynks@gmail.com>
2022-01-21 23:16:56 -0300

    Teste

--- teste/script.js
+++ teste/script.js
@@ -1,20 +1,16 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
-    if (event.type === 'touchstart')
-        event.preventDefault()
+    if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
     const active = nav.classList.contains('active');
     event.currentTarget.setAttribute('aria-expanded', active);
-    if (active)
-
+    if (active) {
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-
-    else {
+    } else {
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
     }
-
 }
 
 btnMobile.addEventListener('click', toggleMenu);
